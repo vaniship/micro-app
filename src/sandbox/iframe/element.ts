abd31e3956f7bcb17ce5af416242f855e2134814
bailicangdu <1264889788@qq.com>
2023-09-12 19:58:17 +0800

    chore: fix husky

--- src/sandbox/iframe/element.ts
+++ src/sandbox/iframe/element.ts
@@ -81,7 +81,6 @@ function patchIframeNode (
     // if (rootNode === appInstanceMap.get(appName)?.container) return microDocument
     // return rootNode
   }
-  console.log(11111)
 
   microRootNode.prototype.appendChild = function appendChild <T extends Node> (node: T): T {
     updateElementInfo(node, appName)
